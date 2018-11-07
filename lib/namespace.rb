df49377bdde9dd7f1224c55386c08d654393221d
Chris Tembreull <ctembreull@users.noreply.github.com>
2018-11-06 16:45:31 -0800

    Create namespace.rb

---
+++ lib/namespace.rb
@@ -0,0 +1,12 @@
+# Used for binding variables into ERB templates.
+class Namespace
+  def initialize(hash)
+    hash.each do |key,value|
+      singleton_class.send(:define_method, key) { value }
+    end
+  end
+
+  def get_binding
+    binding
+  end
+end
